495a35e3dc7538e04f628dfeb2297372707c2aa5
Brooke Norton <nortonbrooke60@gmail.com>
2020-08-26 19:32:28 -0600

    Sign out confirmation

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,5 +1,11 @@
 import React, { useContext } from "react";
-import { SafeAreaView, View, ScrollView, StyleSheet } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  ScrollView,
+  StyleSheet,
+  Alert,
+} from "react-native";
 import Button from "../components/Button";
 import { ThemeContext } from "../contexts/theme";
 import { AuthContext } from "../contexts/auth";
@@ -8,11 +14,19 @@ export default function AccountScreen() {
   const theme = useContext(ThemeContext);
   const { signOut } = useContext(AuthContext);
 
+  const confirmSignOut = () =>
+    Alert.alert(
+      "Sign Out",
+      "Are you sure you want to sign out?",
+      [{ text: "No" }, { text: "Yes", onPress: () => signOut() }],
+      { cancelable: false }
+    );
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: theme.background }}>
       <View style={styles.contentContainer}>
         <ScrollView>
-          <Button onPress={() => signOut()}>Sign out</Button>
+          <Button onPress={() => confirmSignOut()}>Sign out</Button>
         </ScrollView>
       </View>
     </SafeAreaView>
